558143505db65f7bb333a3136869bae656fdba55
GilbertoFreitasJunior <gilberto@frgnet.com.br>
2021-04-30 06:39:46 -0300

    first commit

---
+++ Controllers/MainController.cs
@@ -0,0 +1,49 @@
+﻿using AppExemplo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppExemplo.Controllers
+{
+    public class MainController : Controller
+    {
+        private readonly ILogger<MainController> _logger;
+
+        public MainController(ILogger<MainController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AdicionarProduto()
+        {
+            return View();
+        }
+
+        public IActionResult ListaProdutos()
+        {
+            return View();
+        }
+
+        public IActionResult ProdutoIncluido(Produto produto)
+        {
+            var csv = new CsvHandler();
+            csv.AddProduto(produto);
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CsvHandler.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace AppExemplo
+{
+    public class CsvHandler
+    {
+        public void AddProduto(Produto produto)
+        {
+            using (var file = File.AppendText("Models/produtos.csv"))
+            {
+                file.WriteLine($"{produto.Id},{produto.Nome},{produto.Preco}");
+            }
+        }
+
+        public List<Produto> ListarProdutos()
+        {
+            var Produtos = new List<Produto>();
+            using (var fs = new FileStream("Models/produtos.csv", FileMode.Open))
+            using (var sr = new StreamReader(fs))
+            {
+                while (!sr.EndOfStream)
+                {
+                    var novaLinha = sr.ReadLine();
+                    if (novaLinha != "")
+                    {
+                        string[] linha = novaLinha.Split(',');
+
+                        var id = Int32.Parse(linha[0]);
+                        var nome = linha[1];
+                        var preco = "";
+
+                        if (linha.Length > 3 && !String.IsNullOrEmpty(linha[3]))
+                        {
+                            preco = linha[2] + "," + linha[3];
+                        }else
+                        {
+                            preco = linha[2];
+                        }
+
+                        for (int i = 0; i < Produtos.Count; i++)
+                        {
+                            if (id == Produtos[i].Id)
+                            {
+                                id++;
+                            }
+                        }
+
+                        var produto = new Produto() { Nome = nome, Id = id, Preco = preco };
+                        Produtos.Add(produto);
+                    }
+                }
+            }
+
+            return Produtos;
+        }
+    }
+}
